1819d3a813b77cf43b4d9bce07ebce57b0044fae
Zachary <zachahernandezz@gmail.com>
2024-05-24 22:43:12 -0600

    Test

--- src/test/java/domain/BTreeTest.java
+++ src/test/java/domain/BTreeTest.java
@@ -16,14 +16,13 @@ class BTreeTest {
     void Test() throws TreeException, StackException, QueueException {
         BTree bTree = new BTree();
         SinglyLinkedList singly = new SinglyLinkedList();
-        CircularDoublyLinkedList circular =new CircularDoublyLinkedList();
-        LinkedStack stack= new LinkedStack();
+        CircularDoublyLinkedList circular = new CircularDoublyLinkedList();
+        LinkedStack stack = new LinkedStack();
         LinkedQueue queue = new LinkedQueue();
-        BTree alphabetBTree=new BTree();
+        BTree alphabetBTree = new BTree();
 
 
-
-        for(int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             bTree.add(util.Utility.getRandom(50));
 
 
@@ -34,49 +33,45 @@ class BTreeTest {
 
         try {
             System.out.println(bTree);//chanced it fo a bit bTree.preOrder()
-            System.out.println("Size " +bTree.size());
+            System.out.println("Size " + bTree.size());
             int value = util.Utility.getRandom(50);
             bTree.remove(value);
             if (bTree.contains(value)) {
                 bTree.remove(value);
-                System.out.println("Value: "+value+" has been removed");
-            }else System.out.println("the value ("+value+") was not found"); //TODO fix the remove to properly remove the number
-            System.out.println("tree height: "+bTree.height());
+                System.out.println("Value: " + value + " has been removed");
+            } else
+                System.out.println("the value (" + value + ") was not found");
+            System.out.println("tree height: " + bTree.height());
             System.out.println(bTree);
 
         } catch (TreeException e) {
             throw new RuntimeException(e);
         }
-        //TODO add the height(element) test
 
         bTree.clear();
-        String[] names = {"Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Hank", "Ivy", "Jack",
-                "Kathy", "Leo", "Mona", "Nancy", "Oscar", "Paul", "Quincy", "Rita", "Sam", "Tina",
-                "Uma", "Victor", "Wendy", "Xander", "Yara", "Zane", "Amy", "Brian", "Cynthia", "Derek"};
+        String[] names = {"Zachary", "Lito", "Betzabeth", "Jose David", "Jeaustin", "Eduardo", "Michael", "Katherine", "JP", "Kerlon",
+                "Karla", "Veronica", "Aaron", "Jimena", "Prof.Gilbert", "Felipe", "Niguel", "Dennis", "Joseph", "Alexander"};
 
-        String[] countries = {
-                "Argentina", "Australia", "Belgium", "Brazil", "Canada", "China", "Denmark", "Egypt", "Finland", "France",
-                "Germany", "Greece", "India", "Indonesia", "Italy", "Japan", "Kenya", "Mexico", "Netherlands", "Nigeria",
-                "Norway", "Poland", "Portugal", "Russia", "South Africa", "South Korea", "Spain", "Sweden", "Switzerland", "United States"
-        };
+        String[] countries = {"Argentina", "Chile", "Costa Rica", "Colombia", "Mexico", "Puerto Rico", "Venezuela", "Panama", "Nicaragua", "Honduras"};
+
         char[] alphabet = {
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
         };
         int num;
         for (int i = 0; i < 20; i++) {
             singly.add(util.Utility.getRandom(20));
-            circular.add(names[util.Utility.getRandom(30)-1]);
-            if(i%2==0)
-                stack.push(countries[util.Utility.getRandom(30)-1]);
+            circular.add(names[util.Utility.getRandom(20) - 1]);
+            if (i % 2 == 0)
+                stack.push(countries[util.Utility.getRandom(10) - 1]);
             for (int j = 0; j < 5; j++) {
-                num=util.Utility.getRandom(5000);
-                if (num<1000)
-                    num=num+1000;
+                num = util.Utility.getRandom(5000);
+                if (num < 1000)
+                    num = num + 1000;
                 queue.enQueue(num);
             }
         }
 
-        for (char letter: alphabet) {
+        for (char letter : alphabet) {
             alphabetBTree.add(letter);
         }
 
@@ -91,6 +86,10 @@ class BTreeTest {
         bTree.height(stack);
 
         bTree.remove(queue);
+
+
+        System.out.println();
+
 
         System.out.println(bTree);
 
